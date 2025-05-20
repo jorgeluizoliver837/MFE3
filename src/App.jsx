639419353776ce35f6e86294c72237d34b6c6528
Roberto Alves <roberto_junior_sousa@hotmail.com>
2025-05-20 07:45:15 -0300

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
 
   const handleCount = () => {
     setCount((count) => count + 1);
-    window.dispatchEvent(new CustomEvent('addItem', { detail: count }));
   }
+
+  useEffect(() => {
+    window.dispatchEvent(new CustomEvent('addItem', { detail: count }));
+  }, [count]);
 
   return (
     <>
